ef54f248587424f9779260fffc469fd0aa311138
vidhushan2001 <125108689+vidhushan2001@users.noreply.github.com>
2025-11-13 22:28:26 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -42,8 +42,7 @@ function list_users_with_read_access {
 }
 function helper{
 expected_cmd_args=2
-if [ $# -ne $expected_cmd_args];
-then
+if [ $# -ne $expected_cmd_args]; then
     echo"Please execute the script with required cmd args
 }
 
